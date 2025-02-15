c47d6ce1c132b8b35a7c4f47cb27e199cc318ff2
Pabitrakhadka517 <pratimakhadka517@gmail.com>
2025-02-15 12:46:54 +0545

    BookAppointment Page

--- app/src/main/java/com/example/kotlinapp/ui/activity/BookAppointmentActivity.kt
+++ app/src/main/java/com/example/kotlinapp/ui/activity/BookAppointmentActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.database.FirebaseDatabase
 import java.text.SimpleDateFormat
 import java.util.*
 
+//BookAppointment Page
 class BookAppointmentActivity : AppCompatActivity() {
 
     private lateinit var doctorName: TextView
