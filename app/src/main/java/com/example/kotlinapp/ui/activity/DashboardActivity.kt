602bda95e2a3a0e3bcbd1d426f1d3f410954ed84
Pabitrakhadka517 <pratimakhadka517@gmail.com>
2025-02-28 13:32:19 +0545

    Changes in Dashboard

--- app/src/main/java/com/example/kotlinapp/ui/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/kotlinapp/ui/activity/DashboardActivity.kt
@@ -1,19 +1,11 @@
 package com.example.kotlinapp.ui.activity
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlinapp.Adapter.NearDoctorsAdapter
-import com.example.kotlinapp.R
 import com.example.kotlinapp.databinding.ActivityDashboardBinding
-import com.example.kotlinapp.databinding.ActivitySplashBinding
 import com.example.kotlinapp.viewmodel.MainViewModel
 
 class DashboardActivity : BaseActivity() {
